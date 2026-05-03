8c73e1eb109f8da8870876896bd2589c909b979a
Karan Khanna <kkhanna304@gmail.com>
2026-05-03 21:15:56 +0530

    fix register route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,11 +7,15 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./pages/login/login').then((m) => m.Login)
   },
+  // {
+  //   path: 'register',
+  //   canActivate: [authGuard],
+  //   loadComponent: () => import('./pages/register/register').then((m) => m.Register)
+  // },
   {
-    path: 'register',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/register/register').then((m) => m.Register)
-  },
+  path: 'register',
+  loadComponent: () => import('./pages/register/register').then((m) => m.Register)
+},
   {
     path: 'rooms',
     canActivate: [authGuard],
